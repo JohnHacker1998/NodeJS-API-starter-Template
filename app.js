c82360600a2891cf4b23d5df01b0f72facd34296
Yohannes Kindu <Yohannes.Kindu@gmail.com>
2022-07-13 08:23:00 +0300

    added env integration

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-//require('dotenv').config();
+require('dotenv').config();
 
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 let studentRouter = require('./routes/student')
 
-mongoose.connect('mongodb://localhost:27017/students', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log("Connected to database!!"))
